097d2e22d6d94c949e6841fc564a615b737628fa
Justin <justin.mclellan99@gmail.com>
2019-01-10 16:52:34 -0600

    Fix for editing messages and moved to a function

--- server.js
+++ server.js
@@ -7,145 +7,99 @@ const Humanoid = require("humanoid-js");
 const humanoid = new Humanoid();
 require('dotenv').config()
 // Helpers
-const { mapLeague, removePastRoles, truncate, isValidDate, daysMap } = require('./crawler-helper.js');
+const { mapLeague, removePastRoles, truncate, isValidDate, getMessageErrors, daysMap } = require('./crawler-helper.js');
 
 client.login(process.env.BOT_TOKEN);
 
 client.on('ready', () => {
-  client.user.setUsername("NA Scrims Bot"); 
+  client.user.setUsername("NA Scrims Bot");
 })
 client.on('message', msg => {
   const userId = msg.author.id,
-        userName = msg.author.username,
-        isBot = msg.author.bot,
-        scrimFormatPattern = /(^(((mon|fri|sun)(day)?|tue(sday)?|wed(nesday)?|thu(rsday)?|sat(urday)?) (0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) ((0?[2-9]|1[012])([: ][0-5]?[0-9])? ?([ap]m)?[ ,\/]?)+ ?(inferno|nuke|mirage|dust2|train|overpass|cache|\/)*)$)+/gim
+    userName = msg.author.username,
+    isBot = msg.author.bot
 
-  if(!isBot) {
+  if (!isBot) {
     var c = new Crawler({
-        maxConnections: 10,
-        callback: function (error, res, done) {
-            if (error) {
-                console.log(error);
+      maxConnections: 10,
+      callback: function (error, res, done) {
+        if (error) {
+          console.log(error);
+        } else {
+          var $ = res.$;
+          let rank = $("#rankGraph h1").text().trim(),
+            rRole = $("#rankGraph h1").text().trim().charAt(0) || "Unranked",
+            league = mapLeague($('label:contains("League:")').siblings('.data').text()),
+            rankRole = msg.guild.roles.find(role => role.name.trim() === rRole),
+            leagueRole = msg.guild.roles.find(role => role.name.trim() === league);
+
+          let oldDisplayName = msg.member.displayName,
+            newDisplayName = msg.member.displayName; //default
+
+          if (league == "Advanced") league = "Adv";
+          if (league == "Professional") league = "EPL";
+
+          try {
+            oldDisplayName = $('#upanel-profile header')[0].children[0].data.split('- User')[0].trim();
+            if (league != 'LFT') {
+              let teamName = $('label:contains("League:")').siblings('.data').find('a[href^="/teams"]')[0].children[0].data;
+              if (teamName.length > 12) teamName = truncate(teamName, 12);
+              newDisplayName = `[${teamName}-${league}]`;
+              if (newDisplayName.length > 20) newDisplayName = truncate(newDisplayName, 20) + ']';
+              newDisplayName = `${newDisplayName} ${oldDisplayName}`;
+              if (newDisplayName.length > 32) newDisplayName = truncate(newDisplayName, 30);
+
             } else {
-              var $ = res.$;
-              let rank = $("#rankGraph h1").text().trim(),
-                  rRole = $("#rankGraph h1").text().trim().charAt(0) || "Unranked",
-                  league = mapLeague($('label:contains("League:")').siblings('.data').text()),
-                  rankRole = msg.guild.roles.find(role => role.name.trim() === rRole),
-                  leagueRole = msg.guild.roles.find(role => role.name.trim() === league);
-
-                let oldDisplayName = msg.member.displayName,
-                    newDisplayName = msg.member.displayName; //default
-              
-                if(league == "Advanced") league = "Adv";
-                if(league == "Professional") league = "EPL";
-                
-                try {
-                  oldDisplayName = $('#upanel-profile header')[0].children[0].data.split('- User')[0].trim();
-                  if(league != 'LFT')
-                  {
-                    let teamName = $('label:contains("League:")').siblings('.data').find('a[href^="/teams"]')[0].children[0].data;
-                    if(teamName.length > 12) teamName = truncate(teamName, 12);
-                    newDisplayName =`[${teamName}-${league}]`;
-                    if(newDisplayName.length > 20) newDisplayName = truncate(newDisplayName, 20) + ']';
-                    newDisplayName = `${newDisplayName} ${oldDisplayName}`;
-                    if(newDisplayName.length > 32) newDisplayName = truncate(newDisplayName, 30);
-
-                  } else {
-                    newDisplayName = `[LFT] ${oldDisplayName}`;
-                  }
-                    
-                  msg.member.setNickname(newDisplayName);
-                } catch(e) { msg.reply('Unable to set nickname, check with developer.'); console.log(e); };
-                
-                removePastRoles(rankRole, leagueRole, msg)
-                  .then(() => {
-                      msg.member.addRole(rankRole);
-                      if(leagueRole) msg.member.addRole(leagueRole);
-                      msg.reply('role(s) assigned, thanks!')
-                  });
+              newDisplayName = `[LFT] ${oldDisplayName}`;
             }
-            done();
+
+            msg.member.setNickname(newDisplayName);
+          } catch (e) { msg.reply('Unable to set nickname, check with developer.'); console.log(e); };
+
+          removePastRoles(rankRole, leagueRole, msg)
+            .then(() => {
+              msg.member.addRole(rankRole);
+              if (leagueRole) msg.member.addRole(leagueRole);
+              msg.reply('role(s) assigned, thanks!')
+            });
         }
+        done();
+      }
     });
-    
+
     if (msg.channel.name == "role-assigner") {
-       if (msg.content.indexOf("play.esea.net/users/") > -1) {
-            humanoid.get(msg.content)
-                .then(res => {
-                    c.queue([{
-                        html: res.body
-                    }]);
-                })
-                .catch(err => {
-                    console.error(err)
-                })
-        } 
-//       else if(msg.content.toLowerCase().indexOf("awesome") > -1 || msg.content.toLowerCase().indexOf("great job") > -1 || msg.content.toLowerCase().indexOf("ty") > -1 || msg.content.toLowerCase().indexOf("thx") > -1 || msg.content.toLowerCase().indexOf("nice job") > -1 || msg.content.toLowerCase().indexOf("thanks") > -1 || msg.content.toLowerCase().indexOf("thank you") > -1) {
-//           msg.reply("You're welcome :)"); 
-//         }
-    }else if(msg.channel.name == 'regextest') {
-     let escaped = escape(msg.content).split('%0A'),
-          errors = [];
-          escaped.map(e => {
-              let content = unescape(e).trim();
-              console.log(content);
-            scrimFormatPattern.lastIndex = 0;
-              let regexResult = scrimFormatPattern.test(content);
-              console.log(regexResult);
-              if (!regexResult) {
-                  errors.push(content);
-              } else {
-                let date = content.split(' ')[1].replace('-', '/') + `/${(new Date()).getFullYear()}`,
-                  day = content.split(' ')[0].toLowerCase();
-                if (isValidDate(date)) {
-                    //make sure the date is the same day as the day provided
-                    let _day = daysMap(day);
-                    let dateTest = new Date(date).getDay();
-                    if (_day != dateTest) errors.push(content);
-                } else errors.push(content);
-              }
-
-          });
-
-          if (errors.length > 0) {
-              msg.delete();
-              msg.author.sendMessage(`One or more dates had an invalid format.\n\nPlease use this example to format your message properly.\nTue 1-8 4/5/6/7 mirage/nuke\n\nnumber of errors ${errors.length}\n\noriginal message:\n${msg.content}`);
-          }
+      if (msg.content.indexOf("play.esea.net/users/") > -1) {
+        humanoid.get(msg.content)
+          .then(res => {
+            c.queue([{
+              html: res.body
+            }]);
+          })
+          .catch(err => {
+            console.error(err)
+          })
+      }
+      //       else if(msg.content.toLowerCase().indexOf("awesome") > -1 || msg.content.toLowerCase().indexOf("great job") > -1 || msg.content.toLowerCase().indexOf("ty") > -1 || msg.content.toLowerCase().indexOf("thx") > -1 || msg.content.toLowerCase().indexOf("nice job") > -1 || msg.content.toLowerCase().indexOf("thanks") > -1 || msg.content.toLowerCase().indexOf("thank you") > -1) {
+      //           msg.reply("You're welcome :)"); 
+      //         }
+    } else if (msg.channel.name == 'regextest') {
+      let errors = [];
+      errors = getMessageErrors(msg);
+      if (errors.length > 0) {
+        msg.delete();
+        msg.author.sendMessage(`One or more dates had an invalid format.\n\nPlease use this example to format your message properly.\nTue 1-8 4/5/6/7 mirage/nuke\n\nnumber of errors ${errors.length}\n\noriginal message:\n${msg.content}`);
+      }
     }
   }
 })
 
 client.on('messageUpdate', (oldMessage, newMessage) => {
-  if(newMessage.channel.name == 'regextest') {
-    let result = newMessage.content.match(scrimFormatPattern);
-    if(!result || result.length == 0 || result[0] != newMessage.content){
+  if (oldMessage.channel.name == 'regextest') {
+    let errors = [];
+    errors = getMessageErrors(newMessage);
+    if (errors.length > 0) {
       newMessage.delete();
-      newMessage.author.send(`Invalid format. Please use this example to format your message properly.\nTuesday 4/5/6/7 mirage/nuke\noriginal message: ${newMessage.content}`)
-    }
-    else 
-    {
-      let escaped = escape(newMessage.content).split('%0A'),
-          errors = [];
-      escaped.map(e => {
-        let content = unescape(e);
-        let date = content.split(' ')[1].replace('-','/') + `/${(new Date()).getFullYear()}`,
-            day = content.split(' ')[0].toLowerCase();
-        if(isValidDate(date)) {
-          let _day = daysMap(day);
-          console.log(date);
-          let dateTest = new Date(date).getDay();
-          console.log(_day, dateTest);
-          if(_day != dateTest) errors.push(content);
-        } 
-        else errors.push(content);
-      });
-
-      if(errors.length > 0) {
-        // newMessage.edit(oldMessage.content);
-        newMessage.delete();
-        newMessage.author.send(`One or more dates had an invalid format.\n\nPlease use this example to format your message properly.\nTuesday 4/5/6/7 mirage/nuke\n\nnumber of errors ${errors.length}\n\noriginal message:\n${newMessage.content}`);
-      }    
+      newMessage.author.sendMessage(`One or more dates had an invalid format.\n\nPlease use this example to format your message properly.\nTue 1-8 4/5/6/7 mirage/nuke\n\nnumber of errors ${errors.length}\n\noriginal message:\n${newMessage.content}`);
     }
   }
 })
@@ -159,13 +113,13 @@ var app = express();
 // When the server gets a request for the "root" route of this domain: "/"
 // here: "https://hello-node-server.glitch.me/"
 app.get("/", function (request, response) {
-    // we program the server to respond with an HTML string
-    response.send("<h1>Hello :)</h1><p><a href='/about'>About</a></p>");
+  // we program the server to respond with an HTML string
+  response.send("<h1>Hello :)</h1><p><a href='/about'>About</a></p>");
 });
 
 // when the server gets a request for "https://hello-node-server.glitch.me/about"
 app.get("/about", function (request, response) {
-    response.send("<h1>This is my about page</h1><a href='/'>Home</a></p>");
+  response.send("<h1>This is my about page</h1><a href='/'>Home</a></p>");
 });
 
 // finally we tell the server to listen for requests
