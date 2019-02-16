027200f18927ccb5821a8393bdafa2835fc5cc17
Jordan Dev <jordan.max@loop.colum.edu>
2019-02-16 11:28:41 -0600

    Update server.js

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const {
     checkMessages,
     getTomorrowDate
 } = require('./crawler-helper.js');
+const scrimFormatPattern = /(^(((mon|fri|sun)(day)?|tue(sday)?|wed(nesday)?|thu(rsday)?|sat(urday)?) (0?[1-9]|1[0-2])-(0?[1-9]|[12][0-9]|3[01]) ((0?[2-9]|1[012])([: ][0-5]?[0-9])? ?([ap]m)?[ ,\/]?)+ ?(inferno|nuke|mirage|dust2|train|overpass|cache|\/)*)$)+/gim;
 
 client.login(process.env.BOT_TOKEN);
 
